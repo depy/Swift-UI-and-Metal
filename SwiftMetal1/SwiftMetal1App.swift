43d9d3422e8681f1b0d025aa3fc9714d51ba4faf
RDMengineer <matjazvn@gmail.com>
2021-12-16 13:28:50 +0100

    Initial Commit

---
+++ SwiftMetal1/SwiftMetal1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMetal1App.swift
+//  SwiftMetal1
+//
+//  Created by Matjaz Muhic on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMetal1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
